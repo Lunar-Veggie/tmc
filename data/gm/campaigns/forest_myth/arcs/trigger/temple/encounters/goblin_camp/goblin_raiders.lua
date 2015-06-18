98a9631c71437b542ffe822cbee07a2cc39c96d7
Mark Tibblin <mark.tibblin@gmail.com>
2015-06-18 21:27:19 +0200

    Changed some details in the 'fight off goblin raiders' quest

--- data/gm/campaigns/forest_myth/arcs/trigger/temple/encounters/goblin_camp/goblin_raiders.lua
+++ data/gm/campaigns/forest_myth/arcs/trigger/temple/encounters/goblin_camp/goblin_raiders.lua
@@ -1,16 +1,9 @@
 local GoblinRaiders = class()
 
-function GoblinRaiders:initialize(piece)
-   self._sv.piece = piece
-   self.__saved_variables:mark_changed()
-end
-
 function GoblinRaiders:start(ctx, info)
-   self._ctx = ctx
-   self._info = info
-   assert(ctx.npc_player_id and info.target_player_id)
+   assert(ctx.npc_player_id and ctx.forest_player_id)
 
-   stonehearth.player:set_amenity(ctx.npc_player_id, info.target_player_id, 'hostile')
+   stonehearth.player:set_amenity(ctx.npc_player_id, ctx.forest_player_id, 'hostile')
 end
 
 return GoblinRaiders
