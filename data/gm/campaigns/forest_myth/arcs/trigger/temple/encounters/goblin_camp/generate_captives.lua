6692e925459e725a93b4f974dcae34590e266243
Mark Tibblin <mark.tibblin@gmail.com>
2015-06-20 22:32:04 +0200

    Changed the rescue harpy quest so that there are three smaller harpy cages instead of a big one.

--- data/gm/campaigns/forest_myth/arcs/trigger/temple/encounters/goblin_camp/generate_captives.lua
+++ data/gm/campaigns/forest_myth/arcs/trigger/temple/encounters/goblin_camp/generate_captives.lua
@@ -11,23 +11,29 @@ function GenerateCaptives:start(ctx, info)
    self._ctx = ctx
    self._info = info
 
-   local citizens_by_type = {}
-
-   local player_id = info.captives_player_id
-   local piece_pos = self._sv.piece.position
-   local location = info.from_population.location
-   local population = stonehearth.population:get_population(player_id)
-   local cage_origin = ctx.enemy_location + Point3(piece_pos.x, 0, piece_pos.y) + Point3(location.x, location.y, location.z)
    local ctx_entity_registration_path = info.ctx_entity_registration_path
+   local citizens_by_type = {}
+   citizens_by_type[info.type] = {}
+   local harpy_cages = {
+      ctx[ctx_entity_registration_path].entities.harpy_cage_1,
+      ctx[ctx_entity_registration_path].entities.harpy_cage_2,
+      ctx[ctx_entity_registration_path].entities.harpy_cage_3
+   }
+   local location = info.from_population.location
+   local population = stonehearth.population:get_population(info.captives_player_id)
 
-   local harpies = game_master_lib.create_citizens(population, info, cage_origin, ctx)
+   for id,harpy_cage in pairs(harpy_cages) do
+      local cage_origin = radiant.entities.get_world_location(harpy_cage) + Point3(location.x, location.y, location.z)
 
-   citizens_by_type[info.type] = harpies
+      local harpies = game_master_lib.create_citizens(population, info, cage_origin, ctx)
 
-   for _,harpy in pairs(harpies) do
-      if harpy and harpy:is_valid() then
-         assert(ctx.harpy_captives_camp.entities.harpy_cage and ctx.harpy_captives_camp.entities.harpy_cage:is_valid())
-         harpy:add_component('stonehearth:caged_entity'):set_cage(ctx.harpy_captives_camp.entities.harpy_cage)
+      for _,harpy in pairs(harpies) do
+         table.insert(citizens_by_type[info.type], harpy)
+
+         if harpy and harpy:is_valid() then
+            assert(harpy_cage and harpy_cage:is_valid())
+            harpy:add_component('stonehearth:caged_entity'):set_cage(harpy_cage)
+         end
       end
    end
 
