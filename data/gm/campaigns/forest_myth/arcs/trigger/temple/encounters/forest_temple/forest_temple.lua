35815f68e00fb7e433e76e559680da4b78202c53
Mark Tibblin <mark.tibblin@gmail.com>
2015-08-15 12:45:36 +0200

    Updated to work with Alpha 11.

--- data/gm/campaigns/forest_myth/arcs/trigger/temple/encounters/forest_temple/forest_temple.lua
+++ data/gm/campaigns/forest_myth/arcs/trigger/temple/encounters/forest_temple/forest_temple.lua
@@ -13,24 +13,36 @@ function ForestTempleScript:start(ctx, info)
 end
 
 function ForestTempleScript:_setup_soul_connection(ctx)
-   -- Get a hold of all the entities which has the soul keeper component attached to them
+   -- Cache all of the entities that has a soul keeper component attached to them
    local soul_keepers = {}
-   for id,entity in pairs(ctx.forest_temple.entities) do
+   for _,forest_entities in pairs(ctx.forest_temple.entities) do
 
-      if entity:get_component('tmc:soul_keeper') then
-         entity:get_component('tmc:soul_keeper'):set_id(id)
-         soul_keepers[id] = entity
+      -- We need to make sure that we're working with a table
+      if type(forest_entities) ~= "table" then
+         forest_entities = {forest_entities}
+      end
+
+      for id, entity in pairs(forest_entities) do
+
+         local soul_keeper_comp = entity:get_component('tmc:soul_keeper')
+         if soul_keeper_comp then
+            soul_keeper_comp:set_id(id)
+            soul_keepers[id] = entity
+         end
       end
    end
 
    -- Set all the guards who have the animation component to be connected to every soul keeper
-   for type,citizen_type in pairs(ctx.forest_temple.citizens) do
+   for _,forest_citizens in pairs(ctx.forest_temple.citizens) do
 
-      for id,citizen in pairs(citizen_type) do
+      for _,citizens_type in pairs(forest_citizens) do
 
-         local animation_comp = citizen:get_component('tmc:animation')
-         if animation_comp then
-            animation_comp:set_soul_keepers(soul_keepers)
+         for _,citizen in pairs(citizens_type) do
+
+            local animation_comp = citizen:get_component('tmc:animation')
+            if animation_comp then
+               animation_comp:set_soul_keepers(soul_keepers)
+            end
          end
       end
    end
