948fd3b3d0fe791031c27f649aaa6224e8b809c4
Mark Tibblin <mark.tibblin@gmail.com>
2015-12-22 10:12:48 +0100

    Made an _on_init function instead of having a anonymous one.

--- tmc_server.lua
+++ tmc_server.lua
@@ -3,9 +3,8 @@ local tmc_campaigns = {{type='mythologies', has_combat=true}}
 
 local function start_campaign(campaign)
    if campaign.has_combat then
-      -- We listen in on this only once, when we get this then we know that a new game has been started.
       -- This is a pretty ugly way to disable the campaign, but until Stonehearth makes it easier this will have to do.
-      -- Could use Jelly to make this easier, but should Jelly be required just because of this...?
+      -- Could use Jelly, but should it be required just for this?
       radiant.events.listen_once(stonehearth.personality, 'stonehearth:journal_event', function(args)
          stonehearth.game_master:enable_campaign_type(campaign.type, not stonehearth.game_master._sv.disabled['combat'])
          stonehearth.game_master:_start_campaign(campaign.type)
@@ -16,7 +15,6 @@ local function start_campaign(campaign)
 end
 
 local function set_amenities(data)
-
    for first_player_id, amenities in pairs(data) do
 
       for amenity, second_player_ids in pairs(amenities) do
@@ -29,11 +27,13 @@ local function set_amenities(data)
    end
 end
 
-radiant.events.listen_once(tmc, 'radiant:init', function()
+function tmc:_on_init()
    for _,campaign in pairs(tmc_campaigns) do
       start_campaign(campaign)
    end
    set_amenities(radiant.resources.load_json('tmc:kingdoms:amenities'))
-end)
+end
+
+radiant.events.listen_once(tmc, 'radiant:init', tmc, tmc._on_init)
 
 return tmc
