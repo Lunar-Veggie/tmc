29e9b74b21ae0747a8fb751b55a07d690e0bbe89
Mark Tibblin <mark.tibblin@gmail.com>
2015-12-22 12:08:54 +0100

    Made the dryad into the default role for the forest myth pop. Created the job index and minito'd it to the main mod's own file. Repurposed the spirit and guardian as npc jobs.

--- jobs/npc_jobs/npc_guardian/npc_guardian.lua
+++ jobs/npc_jobs/npc_guardian/npc_guardian.lua
@@ -1,12 +1,12 @@
-local GuardianClass = class()
+local NpcGuardianClass = class()
 local job_helper = radiant.mods.require('stonehearth.jobs.job_helper')
 
-function GuardianClass:initialize(entity)
+function NpcGuardianClass:initialize(entity)
    job_helper.initialize(self._sv, entity)
    self:restore()
 end
 
-function GuardianClass:restore()
+function NpcGuardianClass:restore()
    self._job_component = self._sv._entity:get_component('stonehearth:job')
    if self._sv.is_current_class then
       self:_create_xp_listeners()
@@ -16,68 +16,68 @@ function GuardianClass:restore()
    self.__saved_variables:mark_changed()
 end
 
-function GuardianClass:promote(json, options)
+function NpcGuardianClass:promote(json, options)
    job_helper.promote(self._sv, json)
    self:_create_xp_listeners()
 
    self.__saved_variables:mark_changed()
 end
 
-function GuardianClass:demote()
+function NpcGuardianClass:demote()
    self:_remove_xp_listeners()
    self._sv.is_current_class = false
 
    self.__saved_variables:mark_changed()
 end
 
-function GuardianClass:get_job_level()
+function NpcGuardianClass:get_job_level()
    return self._sv.last_gained_lv
 end
 
-function GuardianClass:is_max_level()
+function NpcGuardianClass:is_max_level()
    return self._sv.is_max_level
 end
 
-function GuardianClass:get_level_data()
+function NpcGuardianClass:get_level_data()
    return self._sv.level_data
 end
 
-function GuardianClass:unlock_perk(id)
+function NpcGuardianClass:unlock_perk(id)
    self._sv.attained_perks[id] = true
 
    self.__saved_variables:mark_changed()
 end
 
-function GuardianClass:get_worker_defense_participation()
+function NpcGuardianClass:get_worker_defense_participation()
    return self._sv.worker_defense_participant
 end
 
-function GuardianClass:has_perk(id)
+function NpcGuardianClass:has_perk(id)
    return self._sv.attained_perks[id]
 end
 
-function GuardianClass:level_up()
+function NpcGuardianClass:level_up()
    job_helper.level_up(self._sv)
 
    self.__saved_variables:mark_changed()
 end
 
-function GuardianClass:_create_xp_listeners()
+function NpcGuardianClass:_create_xp_listeners()
    self._on_attack = radiant.events.listen(self._sv._entity, 'stonehearth:combat:begin_melee_attack', self, self._on_attack)
    self._on_damage_dealt = radiant.events.listen(self._sv._entity, 'stonehearth:combat:melee_attack_connect', self, self._on_damage_dealt)
 end
 
-function GuardianClass:_remove_xp_listeners()
+function NpcGuardianClass:_remove_xp_listeners()
    self._on_attack:destroy()
    self._on_attack = nil
    self._on_damage_dealt:destroy()
    self._on_damage_dealt = nil
 end
 
-function GuardianClass:_on_attack(args)
+function NpcGuardianClass:_on_attack(args)
 end
 
-function GuardianClass:_on_damage_dealt(args)
+function NpcGuardianClass:_on_damage_dealt(args)
    local exp = 0
    if not args.target_exp then
       exp = self._sv.xp_rewards['base_exp_per_hit']
@@ -87,31 +87,31 @@ function GuardianClass:_on_damage_dealt(args)
    self._job_component:add_exp(exp)
 end
 
-function GuardianClass:apply_chained_buff(args)
+function NpcGuardianClass:apply_chained_buff(args)
    radiant.entities.remove_buff(self._sv._entity, args.last_buff)
    radiant.entities.add_buff(self._sv._entity, args.buff_name)
 end
 
-function GuardianClass:apply_buff(args)
+function NpcGuardianClass:apply_buff(args)
    radiant.entities.add_buff(self._sv._entity, args.buff_name)
 end
 
-function GuardianClass:remove_buff(args)
+function NpcGuardianClass:remove_buff(args)
    radiant.entities.remove_buff(self._sv._entity, args.buff_name)
 end
 
-function GuardianClass:add_combat_action(args)
+function NpcGuardianClass:add_combat_action(args)
    job_helper.add_equipment(self._sv, args)
 
    local combat_state = stonehearth.combat:get_combat_state(self._sv._entity)
    combat_state:recompile_combat_actions(args.action_type)
 end
 
-function GuardianClass:remove_combat_action(args)
+function NpcGuardianClass:remove_combat_action(args)
    job_helper.remove_equipment(self._sv, args)
 
    local combat_state = stonehearth.combat:get_combat_state(self._sv._entity)
    combat_state:recompile_combat_actions(args.action_type)
 end
 
-return GuardianClass
+return NpcGuardianClass
--- jobs/npc_jobs/npc_spirit/npc_spirit.lua
+++ jobs/npc_jobs/npc_spirit/npc_spirit.lua
@@ -1,12 +1,12 @@
-local SpiritClass = class()
+local NpcSpiritClass = class()
 local job_helper = radiant.mods.require('stonehearth.jobs.job_helper')
 
-function SpiritClass:initialize(entity)
+function NpcSpiritClass:initialize(entity)
    job_helper.initialize(self._sv, entity)
    self:restore()
 end
 
-function SpiritClass:restore()
+function NpcSpiritClass:restore()
    self._job_component = self._sv._entity:get_component('stonehearth:job')
    if self._sv.is_current_class then
       self:_create_xp_listeners()
@@ -16,56 +16,56 @@ function SpiritClass:restore()
    self.__saved_variables:mark_changed()
 end
 
-function SpiritClass:promote(json, options)
+function NpcSpiritClass:promote(json, options)
    job_helper.promote(self._sv, json)
    self:_create_xp_listeners()
 
    self.__saved_variables:mark_changed()
 end
 
-function SpiritClass:demote()
+function NpcSpiritClass:demote()
    self:_remove_xp_listeners()
    self._sv.is_current_class = false
 
    self.__saved_variables:mark_changed()
 end
 
-function SpiritClass:get_job_level()
+function NpcSpiritClass:get_job_level()
    return self._sv.last_gained_lv
 end
 
-function SpiritClass:is_max_level()
+function NpcSpiritClass:is_max_level()
    return self._sv.is_max_level
 end
 
-function SpiritClass:get_level_data()
+function NpcSpiritClass:get_level_data()
    return nil
 end
 
-function SpiritClass:unlock_perk(id)
+function NpcSpiritClass:unlock_perk(id)
    --self._sv.attained_perks[id] = true
 
    self.__saved_variables:mark_changed()
 end
 
-function SpiritClass:has_perk(id)
+function NpcSpiritClass:has_perk(id)
    return false
 end
 
-function SpiritClass:level_up()
+function NpcSpiritClass:level_up()
    job_helper.level_up(self._sv)
 
    self.__saved_variables:mark_changed()
 end
 
-function SpiritClass:get_worker_defense_participation()
+function NpcSpiritClass:get_worker_defense_participation()
    return self._sv.worker_defense_participant
 end
 
-function SpiritClass:_create_xp_listeners()
+function NpcSpiritClass:_create_xp_listeners()
 end
 
-function SpiritClass:_remove_xp_listeners()
+function NpcSpiritClass:_remove_xp_listeners()
 end
 
-return SpiritClass
+return NpcSpiritClass
