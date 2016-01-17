cc7250d3090340e913ebe6d7c0f91e04a29225c4
Mark Tibblin <mark.tibblin@gmail.com>
2016-01-17 09:17:09 +0100

    Removed empty functions.

--- data/gm/campaigns/forest_myth/arcs/trigger/temple/encounters/quests/failed.lua
+++ data/gm/campaigns/forest_myth/arcs/trigger/temple/encounters/quests/failed.lua
@@ -1,11 +1,5 @@
 local Failed = class()
 
-function Failed:initialize()
-end
-
-function Failed:restore()
-end
-
 function Failed:start(ctx, data)
    radiant.events.trigger_async(ctx.forest_temple.boss, 'tmc:forest_gm:quest:finished', {successful = false})
 end
--- data/gm/campaigns/forest_myth/arcs/trigger/temple/encounters/quests/successful.lua
+++ data/gm/campaigns/forest_myth/arcs/trigger/temple/encounters/quests/successful.lua
@@ -1,11 +1,5 @@
 local Successful = class()
 
-function Successful:initialize()
-end
-
-function Successful:restore()
-end
-
 function Successful:start(ctx, data)
    radiant.events.trigger_async(ctx.forest_temple.boss, 'tmc:forest_gm:quest:finished', {successful = true})
 end
