b234edebd9f13fa88dd5f6135fbc29ce4bd1e4c4
Mark Tibblin <mark.tibblin@gmail.com>
2016-01-17 21:42:01 +0100

    Since equipment comps can be initiated with items, make sure all monsters start off with their choice of weapon that way.

--- entities/forest_myth/golem/golem_init_script.lua
+++
@@ -1,6 +0,0 @@
-local init_fn = function(entity)
-   entity:add_component('stonehearth:equipment')
-            :equip_item('tmc:forest:golem:stone_hands')
-end
-
-return init_fn
--- entities/spiders/arachne/arachne_init_script.lua
+++
@@ -1,6 +0,0 @@
-local init_fn = function(entity)
-   entity:add_component('stonehearth:equipment')
-            :equip_item('tmc:spiders:arachne:scythe')
-end
-
-return init_fn
