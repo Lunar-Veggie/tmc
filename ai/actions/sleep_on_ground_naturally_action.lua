9e6eba88ba126293ceddb999c095f69aee200e77
Mark Tibblin <mark.tibblin@gmail.com>
2016-01-17 15:48:27 +0100

    The dryad can now sleep again! Added a new attribute for the dryad which tells her how long she needs to sleep.

--- ai/actions/sleep_on_ground_naturally_action.lua
+++ ai/actions/sleep_on_ground_naturally_action.lua
@@ -1,24 +1,24 @@
-local SleepOnGroundNaturally = class()
-SleepOnGroundNaturally.name = 'sleep on the ground just as well as anywhere else'
-SleepOnGroundNaturally.does = 'stonehearth:sleep_exhausted'
-SleepOnGroundNaturally.args = {}
-SleepOnGroundNaturally.version = 2
-SleepOnGroundNaturally.priority = 1
+local SleepLib = radiant.mods.require('stonehearth.ai.lib.sleep_lib')
 
-function SleepOnGroundNaturally:run(ai, entity)
+local SleepOnGroundNaturallyAction = class()
+
+SleepOnGroundNaturallyAction.name = 'sleep on the ground just as well as anywhere else'
+SleepOnGroundNaturallyAction.status_text_key = 'stonehearth:ai.actions.status_text.sleep_on_ground'
+SleepOnGroundNaturallyAction.does = 'stonehearth:sleep_exhausted'
+SleepOnGroundNaturallyAction.args = {}
+SleepOnGroundNaturallyAction.version = 2
+SleepOnGroundNaturallyAction.priority = 1
+
+function SleepOnGroundNaturallyAction:run(ai, entity, args)
    ai:execute('stonehearth:drop_carrying_now')
-   ai:execute('stonehearth:run_effect', {effect='yawn'})
-   ai:execute('stonehearth:run_effect', {effect='sit_on_ground'})
-   radiant.entities.add_buff(entity, 'stonehearth:buffs:sleeping');
-   ai:execute('stonehearth:run_effect_timed', {effect='sleep', duration='2h'})
-   radiant.entities.set_attribute(entity, 'sleepiness', stonehearth.constants.sleep.MIN_SLEEPINESS)
+   ai:execute('stonehearth:run_effect', { effect = 'yawn' })
+   ai:execute('stonehearth:run_effect', { effect = 'sit_on_ground' })
+
+   local sleep_duration, rested_sleepiness = SleepLib.get_sleep_parameters(entity, nil)
+   ai:execute('stonehearth:run_sleep_effect', { duration_string = sleep_duration })
+   radiant.entities.set_attribute(entity, 'sleepiness', rested_sleepiness)
+
    radiant.events.trigger_async(entity, 'stonehearth:sleep_on_ground')
 end
 
-function SleepOnGroundNaturally:stop(ai, entity)
-   local name = radiant.entities.get_display_name(entity)
-   stonehearth.events:add_entry(name..' awakes from sleeping on the earth.', 'warning')
-   radiant.entities.remove_buff(entity, 'stonehearth:buffs:sleeping');
-end
-
-return SleepOnGroundNaturally
+return SleepOnGroundNaturallyAction
